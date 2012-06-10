762d527d45c8b5c45e6793cf2eb68cf8da829559
Robin Tweedie <robin@songkick.com>
2012-06-10 18:48:45 +0100

    jshinted

--- clonekick.js
+++ clonekick.js
@@ -1,10 +1,10 @@
 (function (document) {
-  var lineup = document.querySelectorAll('div.line-up ul li');
-  var headliner_names = [], support_names = [];
-  for (i in lineup) {
-    if (lineup[i].children == undefined) { continue; }
-    artist_name = encodeURIComponent(lineup[i].children[0].textContent.trim());
-    if (lineup[i].attributes['class'] == undefined) {
+  var lineup = document.querySelectorAll('div.line-up ul li'),
+      headliner_names = [], support_names = [], i = 0;
+
+  for (i = 0; i < lineup.length; i++) {
+    var artist_name = encodeURIComponent(lineup[i].children[0].textContent.trim());
+    if (lineup[i].attributes['class'] === undefined) {
       support_names.push(artist_name);
     } else {
       headliner_names.push(artist_name);
@@ -13,21 +13,20 @@
 
   var date_string = document.querySelector('div.brief h2').textContent.trim();
   var date = new Date(date_string);
-  var day = date.getDate(),
-      month = date.getMonth() + 1,
+  var day = date.getDate(), month = date.getMonth() + 1,
       year = date.getFullYear();
 
-  var venue_path = document.querySelector('div.location span a').attributes.href.value;
+  var venue_path = document.querySelector('div.location a').attributes.href.value;
   var venue_id = venue_path.split('/').pop();
 
   var base_url = 'http://www.songkick.com/concerts/new?';
   var final_url = base_url.concat('venue_id=' + venue_id);
-  for (i in headliner_names) {
+  for (i = 0; i < headliner_names.length; i++) {
     final_url = final_url + "&event[headliner_names][" + i.toString() + "]=";
     final_url = final_url + headliner_names[i];
   }
 
-  for (i in support_names) {
+  for (i = 0; i < support_names.length; i++) {
     final_url = final_url + "&event[support_names][" + i.toString() + "]=";
     final_url = final_url + support_names[i];
   }
