9adb3c09a36db6e035b2683d8d990e0524a9e18d
Robin Tweedie <robin@songkick.com>
2014-04-26 12:52:47 +0100

    variable naming and declaration cleanup
    Reduce to just one var statement, and convert snake_case variable names to
    camelCase.

--- clonekick.js
+++ clonekick.js
@@ -1,45 +1,46 @@
 (function (document) {
-  var lineup = document.querySelectorAll('div.line-up ul li'),
-      headliner_names = [], support_names = [], i = 0;
+  var baseUrl = 'https://www.songkick.com/concerts/new?',
+      lineup = document.querySelectorAll('div.line-up ul li'),
+      dateString = document.querySelector('div.brief h6 time').attributes.datetime.value,
+      venuePath = document.querySelector('div.location a').attributes.href.value,
+      headlinerNames = [], supportNames = [];
 
   for (i = 0; i < lineup.length; i++) {
-    var artist_name = encodeURIComponent(lineup[i].children[0].textContent.trim());
+    artistName = encodeURIComponent(lineup[i].children[0].textContent.trim());
     if (lineup[i].attributes['class'] === undefined) {
-      support_names.push(artist_name);
+      supportNames.push(artistName);
     } else {
-      headliner_names.push(artist_name);
+      headlinerNames.push(artistName);
     }
   }
 
-  var date_string = document.querySelector('div.brief h4 time').attributes.datetime.value;
-  var date = new Date(date_string);
-  var day = date.getDate(), month = date.getMonth() + 1,
-      year = date.getFullYear(),
-      time = '0' + date.getHours() + ':0' + date.getMinutes();
+  date = new Date(dateString);
+  day = date.getDate();
+  month = date.getMonth() + 1;
+  year = date.getFullYear();
 
+  time = '0' + date.getHours() + ':0' + date.getMinutes();
   time = time.replace(/^0(\d{2}):(?:0(\d{2})|(\d{2}))$/, '$1:$2$3');
 
-  var venue_path = document.querySelector('div.location a').attributes.href.value;
-  var venue_id = venue_path.split('/').pop();
+  venueId = venuePath.split('/').pop();
 
-  var base_url = 'https://www.songkick.com/concerts/new?';
-  var final_url = base_url.concat('venue_id=' + venue_id);
-  for (i = 0; i < headliner_names.length; i++) {
-    final_url = final_url + '&event[headliner_names][' + i.toString() + ']=';
-    final_url = final_url + headliner_names[i];
+  finalUrl = baseUrl.concat('venue_id=' + venueId);
+  for (i = 0; i < headlinerNames.length; i++) {
+    finalUrl = finalUrl + '&event[headliner_names][' + i.toString() + ']=';
+    finalUrl = finalUrl + headlinerNames[i];
   }
 
-  for (i = 0; i < support_names.length; i++) {
-    final_url = final_url + '&event[support_names][' + i.toString() + ']=';
-    final_url = final_url + support_names[i];
+  for (i = 0; i < supportNames.length; i++) {
+    finalUrl = finalUrl + '&event[support_names][' + i.toString() + ']=';
+    finalUrl = finalUrl + supportNames[i];
   }
 
-  final_url = final_url + '&event[date][day]=' + day;
-  final_url = final_url + '&event[date][month]=' + month;
-  final_url = final_url + '&event[date][year]=' + year;
+  finalUrl = finalUrl + '&event[date][day]=' + day;
+  finalUrl = finalUrl + '&event[date][month]=' + month;
+  finalUrl = finalUrl + '&event[date][year]=' + year;
   if (time !== '01:00') {
-    final_url = final_url + '&event[start_time]=' + time;
+    finalUrl = finalUrl + '&event[start_time]=' + time;
   }
 
-  document.location = final_url;
+  document.location = finalUrl;
 }(document));
